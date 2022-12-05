7feb439c735c281dea56e9c517bf34cbaa816647
eduardo <ecapellaturbo@gmail.com>
2022-12-05 18:41:29 -0300

    segundo commit

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,10 +1,11 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"sap/ui/demo/basicTemplate/model/formatter",
-	"sap/m/MessageToast"
+	"../model/formatter",
+	"sap/m/MessageToast",
+	"sap/base/Log"
 ], function(Controller,
 	formatter,
-	MessageToast) {
+	MessageToast, Log) {
 	"use strict";
 
 	return Controller.extend("sap.ui.demo.basicTemplate.controller.App", {
@@ -12,8 +13,19 @@ sap.ui.define([
 		formatter: formatter,
 
 		onInit: function () {
-
+			Log.info("Controller has been initialized.")
 		},
+		onBeforeRendering: function () { 
+			Log.info("The view will shortly be rendered.")
+		},
+		onAfterRendering: function () {
+			 Log.info("The view has been rendered.") 
+			},
+
+		onExit: function(){
+			Log.info("Controller will shortly be destroyed.")
+		},
+
 		onPress: function(sValue){
 			sap.ui.require(["sap/m/MessageToast"], function(oMessage){
 				oMessage.show("Searching...." + sValue);
