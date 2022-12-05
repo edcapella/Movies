9e07ef9c6526cfec3f4a183c9384875f4d2eb757
eduardo capella! <ecapellaturbo@gmail.com>
2022-12-04 21:32:15 -0300

    primeiro commit

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,7 +1,10 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"sap/ui/demo/basicTemplate/model/formatter"
-], function(Controller, formatter) {
+	"sap/ui/demo/basicTemplate/model/formatter",
+	"sap/m/MessageToast"
+], function(Controller,
+	formatter,
+	MessageToast) {
 	"use strict";
 
 	return Controller.extend("sap.ui.demo.basicTemplate.controller.App", {
@@ -10,6 +13,17 @@ sap.ui.define([
 
 		onInit: function () {
 
+		},
+		onPress: function(sValue){
+			sap.ui.require(["sap/m/MessageToast"], function(oMessage){
+				oMessage.show("Searching...." + sValue);
+			});
+		},
+
+		onPressImage: function(oEvent) {
+			sap.ui.require(["sap/m/MessageToast"], function(oMessage){
+				oMessage.show('Do you feel like going to the movies?')
+			})
 		}
 	});
 });
